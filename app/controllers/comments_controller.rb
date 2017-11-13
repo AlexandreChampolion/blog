5220c7be77cc998d7da6e5d409c8b0202286b810
Champolion <a.champolion@show-you.fr>
2017-11-13 13:08:34 +0100

    last commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :find_post
+  
   def new
     @comment = Comment.new
+  end
+
+  def find_post
+    @post = Post.find(params[:post_params])
   end
 end
